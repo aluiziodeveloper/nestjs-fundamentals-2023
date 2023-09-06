e70102a11af1bc503d3fd56607880253f39d14a5
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-05 21:22:16 -0300

    Initial structure

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de cursos' });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `Update course with ID ${id}`;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Delete course with ID ${id}`;
+  }
+}
