235774191c07e102c8128b34cd84215937b04258
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-05 22:09:35 -0300

    Refactoring CoursesController

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -6,9 +6,8 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  Patch,
   Post,
-  Res,
+  Put,
 } from '@nestjs/common'
 import { CoursesService } from './courses.service'
 
@@ -17,29 +16,28 @@ export class CoursesController {
   constructor(private readonly courseService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).json({ message: 'Listagem de cursos' })
+  findAll() {
+    return this.courseService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `Curso com ID ${id}`
+  findOne(@Param('id') id: number) {
+    return this.courseService.findOne(+id)
   }
 
   @Post()
   create(@Body() body) {
-    return body
+    return this.courseService.create(body)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    console.log(body)
-    return `Update course with ID ${id}`
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.courseService.update(+id, body)
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `Delete course with ID ${id}`
+  remove(@Param('id') id: number) {
+    return this.courseService.remove(+id)
   }
 }
