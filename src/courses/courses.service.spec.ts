1efa1f590e06b929eca0153e111d832a5d7dc3f9
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 13:59:53 -0300

    CoursesService unit tests - create method

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,18 +1,71 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CoursesService } from './courses.service';
+import { randomUUID } from 'node:crypto'
+import { CoursesService } from './courses.service'
+import { CreateCourseDTO } from './dto/create-course.dto'
 
-describe('CoursesService', () => {
-  let service: CoursesService;
+describe('CoursesService unit tests', () => {
+  let service: CoursesService
+  let id: string
+  let created_at: Date
+  let expectOutputTags: any
+  let expectOutputCourses: any
+  let mockCourseRepository: any
+  let mockTagRepository: any
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
+    service = new CoursesService()
+    id = randomUUID()
+    created_at = new Date()
+    expectOutputTags = [
+      {
+        id,
+        name: 'nestjs',
+        created_at,
+      },
+    ]
+    expectOutputCourses = {
+      id,
+      name: 'test',
+      description: 'test description',
+      created_at,
+      tags: expectOutputTags,
+    }
 
-    service = module.get<CoursesService>(CoursesService);
-  });
+    mockCourseRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    }
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+      findOne: jest.fn(),
+    }
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  it('should create a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const createCourseDTO: CreateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    }
+
+    const newCourse = await service.create(createCourseDTO)
+
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(newCourse)
+  })
+})
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -8,12 +8,11 @@ import { UpdateCourseDTO } from './dto/update-course.dto'
 
 @Injectable()
 export class CoursesService {
-  constructor(
-    @InjectRepository(Course)
-    private readonly courseRepository: Repository<Course>,
-    @InjectRepository(Tag)
-    private readonly tagRepository: Repository<Tag>,
-  ) {}
+  @InjectRepository(Course)
+  private readonly courseRepository: Repository<Course>
+
+  @InjectRepository(Tag)
+  private readonly tagRepository: Repository<Tag>
 
   async findAll() {
     return this.courseRepository.find({
