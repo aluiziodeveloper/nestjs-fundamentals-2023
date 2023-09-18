ecf99e5680116abbd7772617ab015d5237016a21
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 10:43:57 -0300

    Creating migration AddCoursesIdToCoursesTagsTable

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,16 @@ import { DataSource } from 'typeorm'
 import { dataSourceOptions } from './database.module'
 import { CreateCoursesTable1694705312546 } from 'src/migrations/1694705312546-CreateCoursesTable'
 import { CreateTagsTable1694706531026 } from 'src/migrations/1694706531026-CreateTagsTable'
+import { CreateCoursesTagsTable1695042750118 } from 'src/migrations/1695042750118-CreateCoursesTagsTable'
+import { AddCoursesIdToCoursesTagsTable1695043757120 } from 'src/migrations/1695043757120-AddCoursesIdToCoursesTagsTable'
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1694705312546, CreateTagsTable1694706531026],
+  migrations: [
+    CreateCoursesTable1694705312546,
+    CreateTagsTable1694706531026,
+    CreateCoursesTagsTable1695042750118,
+    AddCoursesIdToCoursesTagsTable1695043757120,
+  ],
 })
