ab946a95d9e32f27aa676a9de39254a83283a657
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 15:48:39 -0300

    e2e tests - create method

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import { Test, TestingModule } from '@nestjs/testing'
-import { CoursesController } from './courses.controller'
 import { INestApplication } from '@nestjs/common'
 import { Course } from './entities/courses.entity'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { Tag } from './entities/tags.entity'
 import { CoursesModule } from './courses.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import request from 'supertest'
 
 describe('CoursesController e2e tests', () => {
   let app: INestApplication
@@ -53,7 +53,22 @@ describe('CoursesController e2e tests', () => {
     await dataSource.destroy()
   })
 
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined()
-  // })
+  afterAll(async () => {
+    await module.close()
+  })
+
+  describe('POST /courses', () => {
+    it('should create a course', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201)
+      expect(res.body.id).toBeDefined()
+      expect(res.body.name).toEqual(data.name)
+      expect(res.body.description).toEqual(data.description)
+      expect(res.body.created_at).toBeDefined()
+      expect(res.body.tags[0].name).toEqual(data.tags[0])
+      expect(res.body.tags[1].name).toEqual(data.tags[1])
+    })
+  })
 })
