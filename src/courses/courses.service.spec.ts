7451d76aac769582d8a9eabc2c392c531fa82435
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 14:15:03 -0300

    CoursesService unit tests - update method

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import { CoursesService } from './courses.service'
 import { CreateCourseDTO } from './dto/create-course.dto'
+import { UpdateCourseDTO } from './dto/update-course.dto'
 
 describe('CoursesService unit tests', () => {
   let service: CoursesService
@@ -90,6 +91,25 @@ describe('CoursesService unit tests', () => {
     const course = await service.findOne(id)
 
     expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(course)
+  })
+
+  it('should update a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const updateCourseDTO: UpdateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    }
+
+    const course = await service.update(id, updateCourseDTO)
+
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(mockCourseRepository.preload).toHaveBeenCalled()
     expect(expectOutputCourses).toStrictEqual(course)
   })
 })
