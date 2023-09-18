720fb41e41fe9dbdc534918f327d2f13879c3c5f
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 14:23:27 -0300

    CoursesService unit tests - remove method

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -112,4 +112,17 @@ describe('CoursesService unit tests', () => {
     expect(mockCourseRepository.preload).toHaveBeenCalled()
     expect(expectOutputCourses).toStrictEqual(course)
   })
+
+  it('should delete a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const course = await service.remove(id)
+
+    expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(mockCourseRepository.remove).toHaveBeenCalled()
+    expect(expectOutputCourses).toStrictEqual(course)
+  })
 })
