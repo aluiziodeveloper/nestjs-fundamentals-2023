a544c905dc0de995438bc743f372db797bc99f70
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-18 15:55:37 -0300

    e2e tests - findAll method

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -71,4 +71,23 @@ describe('CoursesController e2e tests', () => {
       expect(res.body.tags[1].name).toEqual(data.tags[1])
     })
   })
+
+  describe('GET /courses', () => {
+    it('should list all courses', async () => {
+      const res = await request(app.getHttpServer()).get('/courses').expect(200)
+      expect(res.body[0].id).toBeDefined()
+      expect(res.body[0].name).toEqual(data.name)
+      expect(res.body[0].description).toEqual(data.description)
+      expect(res.body[0].created_at).toBeDefined()
+      res.body.map(item =>
+        expect(item).toEqual({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          created_at: item.created_at,
+          tags: [...item.tags],
+        }),
+      )
+    })
+  })
 })
